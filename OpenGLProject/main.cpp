1596ea96cf8865949a05c7f95339deb9bae413cd
Robert <robertjwisniowski@gmail.com>
2022-09-22 19:36:19 +0200

    The triangle is moving thanks to the use of the GLM library.

--- OpenGLProject/main.cpp
+++ OpenGLProject/main.cpp
@@ -6,10 +6,14 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
+#include <glm\glm.hpp>
+#include <glm\gtc\matrix_transform.hpp>
+#include <glm\gtc\type_ptr.hpp>
+
 //Window dimensions
 const GLint WIDTH = 800, HEIGHT = 600;
 
-GLuint VAO, VBO, shader, uniformXMove;
+GLuint VAO, VBO, shader, uniformModel;
 
 bool direction = true;
 float triOffset = 0.0f;
@@ -18,27 +22,27 @@ float triIncrement = 0.005f;
 
 
 // Vertex Shader
-static const char* vShader = "                                          \n\
-#version 330                                                            \n\
-														                \n\
-layout (location = 0) in vec3 pos;                                      \n\
-														                \n\
-uniform float xMove;									                \n\
-														                \n\
-void main()                                                             \n\
-{                                                                       \n\
-	gl_Position = vec4(0.4 * pos.x + xMove, 0.4 * pos.y, 0.4 * pos.z, 1.0);		\n\
+static const char* vShader = "                                                  \n\
+#version 330                                                                    \n\
+														                        \n\
+layout (location = 0) in vec3 pos;                                              \n\
+														                        \n\
+uniform mat4 model; 									                        \n\
+														                        \n\
+void main()                                                                     \n\
+{                                                                               \n\
+	gl_Position = model * vec4(0.4 * pos.x, 0.4 * pos.y, 0.4 * pos.z, 1.0);		\n\
 }";
 
 // Fragment Shader
-static const char* fShader = "                                          \n\
-#version 330                                                            \n\
-														                \n\
-out vec4 colour;                                                        \n\
-														                \n\
-void main()                                                             \n\
-{                                                                       \n\
-	colour = vec4(1.0, 0.0, 0.0, 1.0);	    	                        \n\
+static const char* fShader = "                                                  \n\
+#version 330                                                                    \n\
+														                        \n\
+out vec4 colour;                                                                \n\
+														                        \n\
+void main()                                                                     \n\
+{                                                                               \n\
+	colour = vec4(1.0, 0.0, 0.0, 1.0);	    	                                \n\
 }";
 
 void CreateTriangle()
@@ -125,7 +129,7 @@ void CompileShaders()
         return;
     }
 
-    uniformXMove = glGetUniformLocation(shader, "xMove");
+    uniformModel = glGetUniformLocation(shader, "model");
 }
 
 int main()
@@ -205,7 +209,10 @@ int main()
 
         glUseProgram(shader);
 
-        glUniform1f(uniformXMove, triOffset);
+        glm::mat4 model(1.0f);
+        model = glm::translate(model, glm::vec3(triOffset, triOffset, 0.0f));
+
+        glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
 
         glBindVertexArray(VAO);
         glDrawArrays(GL_TRIANGLES, 0, 3);
